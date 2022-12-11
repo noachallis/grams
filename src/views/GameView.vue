a129b3fd9bb7fd3bdd31c1967068c0478897fb4b
Noa Challis <z5169766@ad.unsw.edu.au>
2022-12-11 23:59:32 +1100

    work

--- src/views/GameView.vue
+++ src/views/GameView.vue
@@ -1,5 +1,6 @@
 <script>
-import { LETTER_DISTRIBUTION } from '../utils/alphabet.js'
+import { LETTER_DISTRIBUTION } from '../utils/letterDistribution.js'
+import { isValidWord } from '../utils/validateWord.js'
 
 const ASCII_A = 65;
 const ASCII_Z = 90;
@@ -8,9 +9,12 @@ export default {
   data() {
     return {
       bag: [],
-      board: [ "E", "N", "A", "P", "I", "S", "C", "I", "V" ],
+      board: [ ],
+      // board: [ "B", "R", "A", "L", "E", "O", "X", "S", "C", "L", "O", "T", "H", "E"],
       p_words: [],
-      input_word: ""
+      p_words: ["BRAND", "HEAR"],
+      input_word: '',
+      selected_word: ''
     }
   },
   methods: {
@@ -27,28 +31,51 @@ export default {
       const tile = this.bag.splice(random, 1)[0];
       this.board.push(tile)
     },
-    enterWord () {
-      this.p_words.push(this.input_word)
+    enterWord() {
+      this.input_word = this.input_word.toUpperCase();
+      if (isValidWord(this.input_word, this.board, this.selected_word, this.p_words)){
+        this.p_words.push(this.input_word)
+      }
       this.input_word = ''
+      this.selected_word = ''
     },
+    selectExistingWord(word) {
+      this.selected_word = word
+    }
   },
   mounted() {
     this.initGame()
+
+    window.addEventListener('keydown', e => {
+      if (e.keyCode >= 65 && e.keyCode <= 90) { // letter pressed
+        this.input_word += String.fromCharCode(e.keyCode);
+      }
+      else if (e.keyCode == 38) { // up arrow
+        this.flipTile();
+      }
+      else if (e.keyCode == 13) { // enter
+        this.enterWord();
+      }
+      else if (e.keyCode == 8 || e.keyCode == 46) { // delete key
+        this.input_word = this.input_word.slice(0, -1); 
+      }
+    });
   }
 }
+
 </script>
 
 <template>
   <v-container >
-
   <v-row>
     <v-col>
-      <v-form @submit.prevent="enterWord">
+      <p>{{input_word}}</p>
+      <!-- <v-form @submit.prevent="enterWord">
         <v-text-field 
           v-model="input_word" 
           variant="underlined">
         </v-text-field>
-      </v-form>
+      </v-form> -->
     </v-col>
     <v-col lg="2">
       <v-btn @click="enterWord" width="170px">Enter Word</v-btn>
@@ -57,18 +84,27 @@ export default {
       <v-btn @click="flipTile" width="170px">Flip Tile</v-btn>
     </v-col>
   </v-row>
-
   <v-row>
       <h1> Board: </h1>
-      <p>
-        {{ board }}
-      </p>
+      <v-container>
+        <v-row>
+          <v-col cols="12" sm="1" md="1" v-for="letter in board" >
+            <div class="tile"><p>{{letter}}</p></div>
+          </v-col>
+        </v-row>
+      </v-container>
   </v-row>
   <v-row>
     <h1> Words: </h1>
-    <p>
-      {{ p_words }}
-    </p>
+    <v-container>
+      <v-row>
+        <v-col cols="12" md="3" v-for="word in p_words" >
+          
+          <v-btn x-large @click="selectExistingWord(word)" ><p>{{word}}</p> </v-btn>
+        </v-col>
+      </v-row>
+    </v-container>
+
   </v-row>
   </v-container>
   <!-- <div class="row">
@@ -78,4 +114,15 @@ export default {
 </template>
 
 <style>
+.tile {
+  background-color: black;
+  color: var(--fg-primary);
+  text-align: center;
+  width: 80%;
+}
+.tile p{
+  color: white;
+  font-weight: bold;
+}
+
 </style>
